d9b621c42d70c761cbc49c282cbf375b8d11d608
Gaál Gergely <contact@gaalgergely.net>
2019-05-11 11:35:49 +0200

    Check if the product belongs to the authorized user when editing it

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,7 @@
 
 use App\Model\Product;
 use Faker\Generator as Faker;
+use App\User;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
@@ -11,6 +12,9 @@ $factory->define(Product::class, function (Faker $faker) {
         'detail' => $faker->paragraph,
         'price' => $faker->numberBetween(100, 1000),
         'stock' => $faker->randomDigit,
-        'discount' => $faker->numberBetween(2,30)
+        'discount' => $faker->numberBetween(2,30),
+        'user_id' => function(){
+            return User::all()->random();
+        }
     ];
 });
