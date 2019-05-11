a5e400cfd787141dfb2a14617d0dd0ddab03d76c
Gaál Gergely <contact@gaalgergely.net>
2019-05-11 10:43:12 +0200

    Customized exception handling with Trait

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+trait ExceptionTrait
+{
+    public function apiException($request, $exception){
+        if($this->isModel($exception)){
+            return response()->json(['errors' => 'Model not found'], Response::HTTP_NOT_FOUND);
+        }
+        if($this->isHttp($exception)){
+            return response()->json(['errors' => 'Incorrect route'], Response::HTTP_NOT_FOUND);
+        }
+        return parent::render($request, $exception);
+    }
+
+    protected function isModel($exception)
+    {
+        return $exception instanceof ModelNotFoundException;
+    }
+
+    protected function isHttp($exception)
+    {
+        return $exception instanceof NotFoundHttpException;
+    }
+}
